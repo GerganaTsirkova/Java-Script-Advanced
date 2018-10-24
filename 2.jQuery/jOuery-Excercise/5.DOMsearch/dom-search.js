9b476b13f9191efd02a705f756b4c0ad2eb3bb47
GerganaTsirkova <33534005+GerganaTsirkova@users.noreply.github.com>
2018-10-24 17:22:43 +0300

    Added better solution of the problem

--- 2.jQuery/jOuery-Excercise/5.DOMsearch/dom-search.js
+++ 2.jQuery/jOuery-Excercise/5.DOMsearch/dom-search.js
@@ -1,44 +1,39 @@
-function domSearch(selectElement, booleanValue) {
-    let selector = $(selectElement);
+function domSearch(selector, booleanValue) {
 
-    let addDiv = $('<div>').addClass('add-controls');
-    let labelAdd = $('<label text="input">Enter text: </label>');
-    let inputAdd = $('<input type="text">');
-    labelAdd.append(inputAdd);
-    let anchorAdd =$('<a class="button" style="display:inline-block">Add</a>');
-    anchorAdd.click(function () {
-        let elementText = $('label input');
-        let newElement = $('<li>')
-            .addClass('list-item')
-            .append($('<a>').addClass('button').text('X').click(function () {
-                $(this).parent().remove();
-            }))
-            .append($('<strong>').text(elementText.val().trim()));
+    $(`${selector}`)
+        .addClass('items-control')
+        .append($('<div class="add-controls">')
+            .append($('<label>Enter text: <input></label>'))
+            .append($('<a class="button" style="display: inline-block;">Add</a>').on('click', function () {
+                let inputText = $('label input');
+                $('.items-list')
+                    .append($('<li class="list-item">')
+                        .append($('<a class="button">X</a>').on('click', function () {
+                            $(this).parent().remove();
+                        }))
+                        .append($(`<strong>${inputText.val().trim()}</strong>`)));
+                inputText.val('')
+            })))
+        .append($('<div class="search-controls">')
+            .append($('<label>Search: <input></label>')))
+        .append($('<div class="result-controls">')
+            .append($('<ul class="items-list">')));
+    //$('li').remove();
 
-        $('ul.items-list').append(newElement);
-        elementText.val('');
-    });
-    addDiv
-        .append(labelAdd)
-        .append(anchorAdd);
+    $('.search-controls :input').on('input propertychange', function () {
+        let searchText = $(".search-controls input");
+        $('li strong').each(function () {
+            if(booleanValue===true){
+                if($(this).text().indexOf(searchText.val().trim())<0){
+                    $(this).parent().css('display','none');
+                }
+            }
+            else {
+                if($(this).text().toLowerCase().indexOf(searchText.val().toLowerCase())<0){
+                    $(this).parent().css('display','none');
+                }
+            }
+        });
 
-
-    let searchDiv = $('<div>').addClass('search-controls');
-    let labelSearch = $('<label text="input">Search: </label>');
-    let inputSearch = $('<input type="text">');
-    searchDiv
-        .append(labelSearch)
-        .append(inputSearch);
-
-
-    let resultDiv = $('<div>').addClass('result-controls');
-    let ul = $('<ul>').addClass('items-list');
-    resultDiv.append(ul);
-
-
-    selector
-        .append(addDiv)
-        .append(searchDiv)
-        .append(resultDiv);
-
+    })
 }
